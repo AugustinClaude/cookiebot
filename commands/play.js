a2b4ba290ce165d215886d6eac66231af6d26b07
Julien Ternet <snoupy18@gmail.com>
2018-10-11 19:25:53 +0200

    bugs

--- commands/play.js
+++ commands/play.js
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
     `▶ **Now Playing** : \`\`\`fix\n${
       info.title
     }\n\`\`\`\n⏳ **Duration** : \`\`\`js\n${ytdl(args[0], {
-      begin: "5s"
+      begin: "30s"
     })} secondes\n\`\`\`\n**Link** : ${args[0]}`
   );
   message.delete();
