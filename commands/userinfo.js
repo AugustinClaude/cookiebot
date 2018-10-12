2c0bfa5edc4299a84c549cc08f0e6fa41cd25968
Julien Ternet <snoupy18@gmail.com>
2018-10-12 21:48:43 +0200

    bugs

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -71,7 +71,7 @@ module.exports.run = async (bot, message, args) => {
         `- ${mentionned.roles
           .filter(role => role.id !== message.guild.id)
           .array()
-          .sort(role => role.id)
+          .sort()
           .map(g => g)
           .join("\n- ")}`,
         true
