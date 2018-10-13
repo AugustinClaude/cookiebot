4017d01ffa6e9ae4b398ab087cf1c41e9328f4a9
Julien Ternet <snoupy18@gmail.com>
2018-10-13 12:29:53 +0200

    bugs

--- commands/hug.js
+++ commands/hug.js
@@ -18,13 +18,13 @@ module.exports.run = async (bot, message, args) => {
       "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur !"
     );
 
-  const { body } = await superagent.get("https://nekos.life/api/v2/img/meow");
+  const { body } = await superagent.get("https://nekos.life/api/v2/img/lizard");
   /*"/img/tickle",  -> guili
   "/img/slap",      -> frapper
   "/img/poke",      -> touche / pousser / enfin il touche un mec ou une meuf pour le **réveiller** ou autre
   "/img/pat",       -> réconforter / ca va aller hein 
   "----------X /img/neko",      -> IMAGE DE MEUF un peu voila mais pas trop ^^ X <- ceci est une croix------
-  "/img/meow",      -> 
+  "/img/meow",      -> CHATS CHATS CHATS ET DES GIF EN PLUS !!!
   "/img/lizard",    -> 
   "/img/kiss",      -> 
   "/img/hug",       -> calin
