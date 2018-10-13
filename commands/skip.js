058fff5be1e2b10bfe4348e438906a142a560da3
Julien Ternet <snoupy18@gmail.com>
2018-10-13 17:08:02 +0200

    bugs

--- commands/skip.js
+++ commands/skip.js
@@ -5,23 +5,19 @@ module.exports.run = async (bot, message, args) => {
   if (!message.member.voiceChannel)
     return message.reply("You're not connect to a voice channel !");
 
-  function play(connection, message) {
-    var server = servers[message.guild.id];
+  const connection = await message.member.voiceChannel.join();
 
-    server.dispatcher = connection.playStream(
-      ytdl(server.queue[0], { filter: "audioonly" })
-    );
+  var server = servers[message.guild.id];
 
-    server.queue.shift();
+  server.dispatcher = connection.playStream(
+    ytdl(server.queue[0], { filter: "audioonly" })
+  );
 
-    server.dispatcher.on("end", function() {
-      if (server.queue[0]) play(connection, message);
-      else connection.disconnect();
-    });
-    if (server.dispatcher) {
-      server.dispatcher.end();
-      message.channel.send("⏭ Song skipped successfully !");
-    }
+  server.queue.shift();
+
+  if (server.dispatcher) {
+    server.dispatcher.end();
+    message.channel.send("⏭ Song skipped successfully !");
   }
 };
 
