ad5bf286976066723b1c2860699e6daacd76c075
Julien Ternet <snoupy18@gmail.com>
2018-10-17 19:46:41 +0200

    bugs

--- commands/membercount.js
+++ commands/membercount.js
@@ -27,7 +27,8 @@ module.exports.run = async (bot, message, args) => {
     nbMember -
     message.guild.members.filter(o => o.presence.status === "offline").size;
 
-  const noRole = roles - role => role.id !== message.guild.id;
+  const everyOne = message.member.has(role => role.id !== message.guild.id);
+  const noRole = Math.floor(Math.random() * roles - everyOne);
 
   /*const noRole = message.guild.member.forEach(member => {
     member.has(role => role.id !== message.guild.id);
