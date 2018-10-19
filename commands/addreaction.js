c1453a9d40babfc53b6f26aa11895f73b2779bf8
Julien Ternet <snoupy18@gmail.com>
2018-10-19 22:40:55 +0200

    bugs

---
+++ commands/addreaction.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  message.delete();
+
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    return message.reply(
+      "Vous n'avez pas les permissions pour exécuter cette commande !"
+    );
+  }
+
+  const messageId = args[0];
+  if (!messageId)
+    return message.reply(
+      "Please provide the ID of a message\n<addreaction [MESSAGE ID] [EMOJI]"
+    );
+
+  const emoji = args[1];
+  if (!emoji)
+    return message.reply(
+      "Please provide a valid emoji !\n<addreaction [MESSAGE ID] [EMOJI]"
+    );
+
+  const msg = args[0];
+  try {
+    await msg.react(args[1]);
+  } catch (e) {
+    message.reply("The message ID or the emoji is incorrect");
+  }
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ar"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: "addreaction"
+};
