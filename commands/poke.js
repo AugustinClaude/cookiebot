4d2ea26f8c9c3fadb3cf8719e8eff2eb0389c18a
Julien Ternet <snoupy18@gmail.com>
2018-10-23 14:40:10 +0200

    bugs

--- commands/poke.js
+++ commands/poke.js
@@ -7,38 +7,45 @@ module.exports.run = async (bot, message, args) => {
     message.mentions.users.first() || message.guild.members.get(args[0])
   );
 
-  if (args[0] === "random") {
-    return message.reply(
-      "Random is not working yet ! Please, mention users :)"
-    );
-  }
+  var random;
 
-  if (!pokeUser)
+  if (!args[0])
+    return message.reply(
+      "An error has occured! Exemple : <poke @user **OR** <poke random !"
+    );
+
+  if (args[0] === "random") {
+    var random = message.guild.members.random().user.username;
+  } else if (pokeUser) var random = pokeUser.user.username;
+
+  if (args[0] !== "random" && !pokeUser)
     return message.channel.send(
-      "This user doesn't exist or you do not have mentionned a user !"
+      "This user doesn't exist or you don't have pinged any users ! Or maybe you didn't write correctly the word : \"__random__\""
     );
 
   const { body } = await superagent.get("https://nekos.life/api/v2/img/poke");
 
-  if (message.author === pokeUserAuto) {
+  try {
+    if (message.author === pokeUserAuto) {
+      const pokeEmbed = new Discord.RichEmbed()
+        .setTitle(`**${message.author.username}** poked himself O_o Ok...`)
+        .setImage(body.url)
+        .setColor("RANDOM");
+
+      return message.channel.send(pokeEmbed);
+    }
+
     const pokeEmbed = new Discord.RichEmbed()
-      .setTitle(`**${message.author.username}** poked himself O_o Ok...`)
+      .setTitle(`**${message.author.username}** poked **${random}** ! `)
       .setImage(body.url)
       .setColor("RANDOM");
 
     return message.channel.send(pokeEmbed);
+  } catch (e) {
+    message.channel.send(
+      "An error has occured and I can't execute this command ! Maybe the syntax is incorrect or the used API don't work !"
+    );
   }
-
-  const pokeEmbed = new Discord.RichEmbed()
-    .setTitle(
-      `**${message.author.username}** poked **${
-        message.mentions.users.first().username
-      }** ! `
-    )
-    .setImage(body.url)
-    .setColor("RANDOM");
-
-  return message.channel.send(pokeEmbed);
 };
 
 module.exports.conf = {
