20808521b8e9ccc2e7263ee35c1cbbf148304129
Julien Ternet <snoupy18@gmail.com>
2018-10-23 14:33:40 +0200

    bugs

--- commands/cuddle.js
+++ commands/cuddle.js
@@ -7,42 +7,53 @@ module.exports.run = async (bot, message, args) => {
     message.mentions.users.first() || message.guild.members.get(args[0])
   );
 
-  if (args[0] === "random") {
-    return message.reply(
-      "Le Random ne fonctionne pas encore ! Merci de mentionner :)"
-    );
-  }
+  var random;
 
-  if (!cuddleUser)
+  if (!args[0])
+    return message.reply(
+      "Syntaxe incorrecte ! Exemple : <cuddle @membre **OU** <cuddle random !"
+    );
+
+  if (args[0] === "random") {
+    var random = message.guild.members.random().user.username;
+  } else if (cuddleUser) var random = cuddleUser.user.username;
+
+  if (args[0] !== "random" && !cuddleUser)
     return message.channel.send(
-      "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur !"
+      "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur ! Ou alors vous avez mal orthographié \"__random__\""
     );
 
   const { body } = await superagent.get("https://nekos.life/api/v2/img/cuddle");
 
-  if (message.author === cuddleUserAuto) {
+  try {
+    if (message.author === cuddleUserAuto) {
+      const cuddleEmbed = new Discord.RichEmbed()
+        .setTitle(
+          `**${
+            message.author.username
+          }** se réconforte lui même O_o Toujours aussi bizarre ce gars ;-;`
+        )
+        .setImage(body.url)
+        .setColor("RANDOM");
+
+      return message.channel.send(cuddleEmbed);
+    }
+
     const cuddleEmbed = new Discord.RichEmbed()
       .setTitle(
         `**${
           message.author.username
-        }** se réconforte lui même O_o Toujours aussi bizarre ce gars ;-;`
+        }** réconforte **${random}** ! Trop mignon :heart:`
       )
       .setImage(body.url)
       .setColor("RANDOM");
 
     return message.channel.send(cuddleEmbed);
+  } catch (e) {
+    message.channel.send(
+      "Une erreur est survenue et il m'est impossible d'exécuter cette commande ! Peut-être que la syntaxe est incorrecte ou alors l'API utilisée ne fonctionne pas !"
+    );
   }
-
-  const cuddleEmbed = new Discord.RichEmbed()
-    .setTitle(
-      `**${message.author.username}** réconforte **${
-        message.mentions.users.first().username
-      }** ! Trop mignon :heart:`
-    )
-    .setImage(body.url)
-    .setColor("RANDOM");
-
-  return message.channel.send(cuddleEmbed);
 };
 
 module.exports.conf = {
