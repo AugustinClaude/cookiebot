d289d040cbf6d31b47243e36b8779083e57a632e
Julien Ternet <snoupy18@gmail.com>
2018-10-23 14:02:10 +0200

    bugs

--- commands/slap.js
+++ commands/slap.js
@@ -7,40 +7,51 @@ module.exports.run = async (bot, message, args) => {
     message.mentions.users.first() || message.guild.members.get(args[0])
   );
 
-  if (args[0] === "random") {
-    return message.reply(
-      "Le Random ne fonctionne pas encore ! Merci de mentionner :)"
-    );
-  }
+  var random;
 
-  if (!slapUser)
+  if (!args[0])
+    return message.reply(
+      "Syntaxe incorrecte ! Exemple : <slap @membre **OU** <slap random !"
+    );
+
+  if (args[0] === "random") {
+    var random = message.guild.members.random().user.username;
+  } else if (slapUser) var random = slapUser.user.username;
+
+  if (args[0] !== "random" && !slapUser)
     return message.channel.send(
-      "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur !"
+      "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur ! Ou alors vous avez mal orthographié \"__random__\""
     );
 
   const { body } = await superagent.get("https://nekos.life/api/v2/img/slap");
 
-  if (message.author === slapUserAuto) {
+  try {
+    if (message.author === slapUserAuto) {
+      const slapEmbed = new Discord.RichEmbed()
+        .setTitle(
+          `**${message.author.username}** s'est frappé lui même O_o D'accord.`
+        )
+        .setImage(body.url)
+        .setColor("RANDOM");
+
+      return message.channel.send(slapEmbed);
+    }
+
     const slapEmbed = new Discord.RichEmbed()
       .setTitle(
-        `**${message.author.username}** s'est frappé lui même O_o D'accord.`
+        `**${
+          message.author.username
+        }** a frappé **${random}** ! C'est vraiment méchant 😕 !`
       )
       .setImage(body.url)
       .setColor("RANDOM");
 
     return message.channel.send(slapEmbed);
+  } catch (e) {
+    message.channel.send(
+      "Une erreur est survenue et il m'est impossible d'exécuter cette commande ! Peut-être que la syntaxe est incorrecte ou alors l'API utilisée ne fonctionne pas !"
+    );
   }
-
-  const slapEmbed = new Discord.RichEmbed()
-    .setTitle(
-      `**${message.author.username}** a frappé **${
-        message.mentions.users.first().username
-      }** ! C'est vraiment méchant 😕 !`
-    )
-    .setImage(body.url)
-    .setColor("RANDOM");
-
-  return message.channel.send(slapEmbed);
 };
 
 module.exports.conf = {
