649de2aefb484e69be9c23b8aee547abac39b6e3
Julien Ternet <snoupy18@gmail.com>
2018-10-23 13:34:53 +0200

    bugs

--- commands/hug.js
+++ commands/hug.js
@@ -17,6 +17,11 @@ module.exports.run = async (bot, message, args) => {
     var random = message.guild.members.random().user.username;
   } else var random = hugUser.user.username;
 
+  if (args[0] !== "random" && !hugUser)
+    return message.channel.send(
+      "L'utilisateur n'existe pas ou vous n'avez mentionner aucun utilisateur !"
+    );
+
   const { body } = await superagent.get("https://nekos.life/api/v2/img/hug");
 
   try {
@@ -43,7 +48,7 @@ module.exports.run = async (bot, message, args) => {
     return message.channel.send(hugEmbed);
   } catch (e) {
     message.channel.send(
-      "Une erreur est survenue et il m'est impossible d'exécuter cette commande ! Vous avez peut-être mentionné un utilisateur qui n'existe pas, ou alors la syntaxe est incorrecte ! Il est possible également que l'API utilisée ne fonctionne pas !"
+      "Une erreur est survenue et il m'est impossible d'exécuter cette commande ! Peut-être que la syntaxe est incorrecte ou alors l'API utilisée ne fonctionne pas !"
     );
   }
 };
