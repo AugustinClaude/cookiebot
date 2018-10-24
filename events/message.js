6df0675068b48e52eaef1a552bd7f99ab6a20a82
Julien Ternet <snoupy18@gmail.com>
2018-10-24 19:36:34 +0200

    bugs

--- events/message.js
+++ events/message.js
@@ -141,7 +141,7 @@ module.exports = (bot, message) => {
 
   //NOMBRE A DEVINER (justeprix)
   if (partyLaunch == true) {
-    if (Number.isInteger(parseInt(message.content))) {
+    if (!isNaN(message.content)) {
       if (message.content > numberRandom) {
         message.reply(
           ":x: Non ! Mauvaise réponse !\nLe vrai prix est plus **PETIT** ⬇ !\nEssaie encore 😉"
