faac20721388850c4bf800d5cf0c251d87a8c4a5
Julien Ternet <snoupy18@gmail.com>
2018-10-26 20:14:50 +0200

    bugs

--- commands/info.js
+++ commands/info.js
@@ -3,14 +3,23 @@ const moment = require("moment");
 
 module.exports.run = async (bot, message, args) => {
   moment.locale("fr");
-    
+
   const servIcon = message.guild.iconURL;
   const servName = message.guild.name;
   //let emojis = message.guild.emojis.map(e => e).join(' | ')
-  const lastmember = Array.from(message.channel.guild.members.values()).sort((a, b) => b.joinedAt - a.joinedAt).map(m => `<@!${m.id}>`).splice(0, 1);
+  const lastmember = Array.from(message.channel.guild.members.values())
+    .sort((a, b) => b.joinedAt - a.joinedAt)
+    .map(m => `<@!${m.id}>`)
+    .splice(0, 1);
   const userJoin = moment(message.member.joinedAt).format("Do MMMM YYYY, LTS");
-  const servCreate = moment(message.guild.createdAt).format("Do MMMM YYYY, LTS");
-  const emojisSize = message.guild.emojis.size
+  const servCreate = moment(message.guild.createdAt).format(
+    "Do MMMM YYYY, LTS"
+  );
+  const emojisSize = message.guild.emojis.size;
+  const textChannels = message.guild.channels.filter(m => m.type == "text")
+    .size;
+  const voiceChannels = message.guild.channels.filter(i => i.type == "voice")
+    .size;
 
   const servEmbed = new Discord.RichEmbed()
     .setAuthor(servName, servIcon)
@@ -21,15 +30,27 @@ module.exports.run = async (bot, message, args) => {
     .addField("🌐 Nom du serveur", servName, true)
     .addField("🏆 Fondateur", message.guild.owner, true)
     .addBlankField()
-    .addField("🥝 Nombre de membres", `**${message.guild.memberCount}** | <mc pour les détails`, true)
+    .addField(
+      "🥝 Nombre de membres",
+      `**${message.guild.memberCount}** | <mc pour les détails`,
+      true
+    )
     .addField(`🌎 ${servName} créé le`, servCreate, true)
     .addBlankField()
-    .addField(`📚 Nombre de channels [${message.guild.channels.size}]`, `💬 Channels textuels : ${}\n🔊 Channels vocaux : ${}`, true)
-    .addField("📜 Nombre d'émojis", `**${emojisSize}** | <emoji pour les détails`, true)
+    .addField(
+      `📚 Nombre de channels [${message.guild.channels.size}]`,
+      `💬 Channels textuels : ${textChannels}\n🔊 Channels vocaux : ${voiceChannels}`,
+      true
+    )
+    .addField(
+      "📜 Nombre d'émojis",
+      `**${emojisSize}** | <emoji pour les détails`,
+      true
+    )
     .addBlankField()
     .addField("🔘 Dernier membre", lastmember, true)
     .addField("🚪 Votre arrivée sur le serveur", userJoin, true)
-    .addBlankField()
+    .addBlankField();
   //.addField(`📜 Liste des émojis [${emojisSize}]`, emojis) <-- TROP VOLUMINEUX (trop d'émojis)
 
   return message.channel.send(servEmbed);
