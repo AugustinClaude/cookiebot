a5b9dba0d17b6f13839c1fe5841db9a257c3eadc
Julien Ternet <snoupy18@gmail.com>
2018-10-29 13:44:46 +0100

    bugs

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ bot.on("guildMemberRemove", async member => {
   const canvas = Canvas.createCanvas(700, 250);
   const ctx = canvas.getContext("2d");
 
-  const background = await Canvas.loadImage("./wallpaper.jpg");
+  const background = await Canvas.loadImage("./wallpaper.png");
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
   const attachment = new Discord.Attachment(
