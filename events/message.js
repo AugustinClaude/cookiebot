8064763830f0eacea0b29e5f1d2349b4ded78ca1
Julien Ternet <snoupy18@gmail.com>
2018-11-10 11:29:11 +0100

    bugs

--- events/message.js
+++ events/message.js
@@ -160,41 +160,4 @@ module.exports = (bot, message) => {
       }
     }
   }
-
-  if (command === "<test") {
-    const reportedUser = message.guild.member(
-      message.mentions.users.first() || message.guild.members.get(args[O])
-    );
-    if (!reportedUser) {
-      return message.channel.send("L'utilisateur n'existe pas !");
-    }
-
-    const reportedReason = args.join(" ").slice(22);
-    if (!reportedReason) {
-      return message.reply("Merci de préciser une raison!");
-    }
-
-    const reportEmbed = new Discord.RichEmbed()
-      .setDescription("Reports")
-      .setColor("#FF0000")
-      .addField(
-        "L'utilisateur reporté",
-        `${reportedUser} (ID : ${reportedUser.id})`
-      )
-      .addField(
-        "L'utilisateur ayant reporté",
-        `${message.author} (ID : ${message.autor.id})`
-      )
-      .addField("Canal", message.channel)
-      .addField("Raison", reportedReason);
-
-    const reportChannel = message.guild.channels.find("name", "reports");
-    if (!reportChannel) {
-      return message.channel.send(
-        "Canal 'Reports' introuvable, Veuillez créer ce canal !"
-      );
-    }
-    message.delete();
-    reportChannel.send(reportEmbed);
-  }
 };
