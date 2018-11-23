45caa7021155d07b37f1fb1d0110d2b8a8949130
Julien Ternet <snoupy18@gmail.com>
2018-11-23 22:09:33 +0100

    bugs

--- events/ready.js
+++ events/ready.js
@@ -14,16 +14,6 @@ module.exports = async bot => {
     "Prêt!"
   );
 
-  bot.emit(
-    "guildMemberAdd"
-  );
-
-  await wait(3000);
-
-  bot.emit(
-    "guildMemberRemove"
-  );
-
   bot.user.setPresence({
     game: {
       name: `<help | ${bot.guilds.size} guilds`,
