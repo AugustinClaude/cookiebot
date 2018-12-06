47415fffff6e0286b07df4d61d3376755483356f
Julien Ternet <snoupy18@gmail.com>
2018-12-06 15:56:54 +0100

    bugs

---
+++ commands/realname.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  message.delete();
+  const mentionned = message.guild.member(
+    message.mentions.users.first() || message.guild.members.get(args[0])
+  );
+
+  message.channel.send(
+    `Le vrai nom de ${mentionned.user.name} est ${mentionned.user.username} !`
+  );
+
+  message.channel.send(
+    `${mentionned.user.username} n'a pas de surnom, c'est son nom d'origine !`
+  );
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["rname", "rn"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: "realname"
+};
