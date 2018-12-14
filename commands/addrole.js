de27935bc54093398f120df471d6d180815c55ba
Julien Ternet <snoupy18@gmail.com>
2018-12-14 19:50:53 +0100

    bugs

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  message.delete();
   const mentionned = message.guild.member(
     message.mentions.users.first() || message.guild.members.get(args[0])
   );
