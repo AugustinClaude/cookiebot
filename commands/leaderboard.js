b02aeb5930c55dc7842c257547b71f3ac76e8de8
Julien Ternet <snoupy18@gmail.com>
2018-12-14 16:20:01 +0100

    bugs

---
+++ commands/leaderboard.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  message.delete();
+
+  const filtered = bot.points.filter(p => p.guild === message.guild.id).array();
+  const sorted = filtered.sort((a, b) => a.points - b.points);
+  const top10 = sorted.splice(0, 10).reverse();
+  const embed = new Discord.RichEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(bot.user.username, bot.user.avatarURL)
+    .setDescription("Voici le Top 10 des joueurs ayant le plus d'xp !")
+    .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
+    .setColor(0x00ae86);
+  for (const data of top10) {
+    embed.addField(
+      bot.users.get(data.user).tag,
+      `Niveau ${data.level} (${data.points} points)`
+    );
+  }
+  return message.channel.send(embed);
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["lead", "lb"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: "leaderboard"
+};
--- commands/level.js
+++ commands/level.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  message.delete();
   const key = `${message.guild.id}-${message.author.id}`;
-  return message.channel.send(
+  return message.reply(
     `Tu es niveau ${bot.points.get(key, "level")} avec ${bot.points.get(
       key,
       "points"
