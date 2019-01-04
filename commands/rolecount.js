6a414a42539c1e56f16261cb92adf0e197833e7b
Julien Ternet <snoupy18@gmail.com>
2019-01-04 19:40:37 +0100

    bugs

--- commands/rolecount.js
+++ commands/rolecount.js
@@ -7,17 +7,14 @@ module.exports.run = async (bot, message, args) => {
       `Syntaxe incorrecte ! Vous devez marquer \`<rolecount [nom d'un role]\``
     );
 
-  const servIcon = message.guild.iconURL;
-  const servName = message.guild.name;
   const roleID = args[0].slice(3, -1);
   const role = message.guild.roles.get(roleID).members;
 
   const servEmbed = new Discord.RichEmbed()
-    .setAuthor(servName, servIcon)
     .setColor("RANDOM")
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setTimestamp()
-    .addField(`${message.guild.roles.get(roleID)}`, role.size + "membres");
+    .addField("  ", `**__${message.guild.roles.get(roleID)} => __ ${role.size}** membres`);
 
   return message.channel.send(servEmbed);
 };
