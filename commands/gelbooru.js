61ae5f3b69d33ae16e335cf2f08c3c4f99ea63bf
Julien Ternet <snoupy18@gmail.com>
2019-01-05 21:19:11 +0100

    bugs

--- commands/gelbooru.js
+++ commands/gelbooru.js
@@ -6,14 +6,14 @@ module.exports.run = async (bot, message, args) => {
   if (message.channel.nsfw === false) {
     return message.channel.send("Vous ne pouvez pas utiliser de commandes NSFW dans ce channel ! Il faut qu'il sois défini comme étant NSFW !");
   }
-
   const args2 = message.content.split(" ");
   args2.shift();
+  
 
   try {
     const gbEmbed = new Discord.RichEmbed()
       .setColor("RANDOM")
-      .setTitle(`🍆 Gelbooru Tag: ${args2.join(" ")}`)
+      .setTitle(`🍆 Gelbooru Tag:  ${args2.join(" ")}`)
       .setImage(`https://www.gelbooru.com/index.php?page=post&s=list&tags=${args2.join("+")}`);
 
     await message.channel.send(gbEmbed);
--- commands/rolecount.js
+++ commands/rolecount.js
@@ -7,14 +7,16 @@ module.exports.run = async (bot, message, args) => {
       `Syntaxe incorrecte ! Vous devez marquer \`<rolecount [nom d'un role]\``
     );
 
-  const roleID = args[0].slice(3, -1);
+  const roleStep = `\\${args[0]}`;
+  message.channel.send(roleStep);
+  const roleID = roleStep.slice(3, -1);
   const role = message.guild.roles.get(roleID).members;
 
   const servEmbed = new Discord.RichEmbed()
     .setColor("RANDOM")
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setTimestamp()
-    .addField("Rôle", `**__${message.guild.roles.get(roleID)}__**`)
+    .addField("Rôle", `**__${message.guild.roles.get(roleID).members}__**`)
     .addField("Membres", `**${role.size}**`);
 
   return message.channel.send(servEmbed);
