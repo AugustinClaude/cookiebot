573a2d967905a84ff47e32dcd31a8bab74fd8834
Julien Ternet <snoupy18@gmail.com>
2019-01-19 13:42:33 +0100

    bugs

--- commands/test.js
+++ commands/test.js
@@ -4,11 +4,18 @@ module.exports.run = async (bot, message, args) => {
 
   message.channel.send("Voulez-vous vraiment me **déconnecter** ?");
 
-  if (message.content === "Oui" || message.content === "oui" || message.content === "y") {
+  if (message.content == "Oui" || message.content === "y") {
     console.log("Déconnexion...");
     message.channel.send("Déconnexion effectué !");
     console.log(`[!] Boulanger est déconnecté par ${message.author.username}!`);
   }
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["mc"],
+  permLevel: 0
 };
 
 module.exports.help = {
