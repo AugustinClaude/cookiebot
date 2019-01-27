71811c67f2ee089a9bd23b5d58c1369cb512945c
Julien Ternet <snoupy18@gmail.com>
2019-01-27 20:30:13 +0100

    bugs

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,14 +4,32 @@ module.exports.run = async (bot, message, args) => {
   message.delete();
   if (message.author.id !== "302901933419790347") return message.reply(":x: Vous n'êtes pas le créateur du bot, vous ne pouvez donc pas utiliser cette commande !");
   else {
-    const m = await message.channel.send("⚙ Redémarrage en cours...");
+    const m = await message.channel.send("⚙ Redémarrage en cours");
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours .");
+    }, 500);
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours . .");
+    }, 500);
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours . . .");
+    }, 500);
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours . .");
+    }, 500);
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours .");
+    }, 500);
+    setTimeout(() => {
+      m.edit("⚙ Redémarrage en cours");
+    }, 500);
     
     bot.destroy();
     bot.login(process.env.BOT_TOKEN);
 
     setTimeout(() => {
       m.edit("⚙ Redémarrage terminé avec succès !");
-    }, 3000);
+    }, 3500);
   }
 };
 
