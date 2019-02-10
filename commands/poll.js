1511d53e574d182d57169512ad4959a86c8ff0dd
Julien Ternet <snoupy18@gmail.com>
2019-02-10 22:52:23 +0100

    bugs

--- commands/poll.js
+++ commands/poll.js
@@ -1,13 +1,17 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  message.delete();
   if (!args[0]) return message.channel.send("Syntaxe : <poll 'Question'");
 
   const pollEmbed = new Discord.RichEmbed()
     .setTitle(`❔ Sondage créé par ${message.author.username} ❔`)
     .setColor("#3ac5a9")
-  //.setFooter("✅ Appuyez sur l'une des réactions ci-dessous ❌")
-    .addField("📖 " + args.join(" "), "✅ Appuyez sur l'une des réactions ci-dessous ❌")
+    //.setFooter("✅ Appuyez sur l'une des réactions ci-dessous ❌")
+    .addField(
+      "📖 " + args.join(" "),
+      "✅ Appuyez sur l'une des réactions ci-dessous ❌"
+    )
     .setFooter("Ceci est un prototype, à améliorer dans le futur!")
     .setTimestamp();
 
