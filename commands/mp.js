87ac38771219699c4475bfd2e2e9c9372b96a2a1
Julien Ternet <snoupy18@gmail.com>
2019-03-17 19:31:12 +0100

    bugs

--- commands/mp.js
+++ commands/mp.js
@@ -3,7 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   message.delete();
   const member = message.guild.members.get("302901933419790347");
-  member.send(args.join(" "));
+  if (!args) return message.reply("vous devez écrire un message !");
+
+  const embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setColor("RANDOM")
+    .setDescription(args.join(" "))
+    .setFooter(
+      "Ce bot a été créé par Spokloo#7791",
+      "https://i.imgur.com/C7cjSEe.png"
+    )
+    .setTimestamp();
+  member.send(embed);
 
   message.reply("votre message a bien été envoyé à mon créateur !");
 };
