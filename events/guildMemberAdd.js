6beb52e717882e9a349f140ac8648312a154dd10
Julien Ternet <snoupy18@gmail.com>
2019-03-18 21:28:25 +0100

    bugs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = (bot, member) => {
-  member.createDM().then(function(channel) {
+  /*member.createDM().then(function(channel) {
     return channel.send(
       `Welcome to \`\`${member.guild.name}\`\` <@${
         member.id
       }> ! Don't forget to check the rules !`
     );
-  });
+  });*/
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,9 +1,9 @@
 module.exports = (bot, member) => {
-  member.createDM().then(function(channel) {
+  /*member.createDM().then(function(channel) {
     return channel.send(
       `Goodbye <@${member.id}> ! You can come back in \`\`${
         member.guild.name
       }\`\` at anytime !\nJust don't forget the link ! :D`
     );
-  });
+  });*/
 };
