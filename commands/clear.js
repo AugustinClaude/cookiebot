87e0f6bf600e2973afdaeac31c9d4677b53ea696
Julien Ternet <snoupy18@gmail.com>
2019-04-16 23:11:50 +0200

    bugs

--- commands/clear.js
+++ commands/clear.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if (
-    !message.member.hasPermission("MANAGE_MESSAGES") ||
+    !message.member.hasPermission("MANAGE_MESSAGES") &&
     message.member.id !== "302901933419790347"
   )
     return message.reply(
