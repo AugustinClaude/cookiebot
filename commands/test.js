d46e01a16c6743c1d455b030d0bcf0f56c857af5
Julien Ternet <snoupy18@gmail.com>
2019-04-30 22:39:51 +0200

    bugs

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   message.delete();
+  message.guild.members.get(bot.user.id).setNickname(args[0]);
 };
 
 module.exports.conf = {
