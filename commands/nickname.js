c4e72bf217ab0755b53f0ca4d428cc43005f8e3b
Julien Ternet <snoupy18@gmail.com>
2019-04-30 23:16:37 +0200

    bugs

--- commands/nickname.js
+++ commands/nickname.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   message.delete();
-  message.guild.members.get(args[0]).setNickname(args.slice(args[0]));
+  message.guild.members.get(args[0]).setNickname(args.slice(args[0]).join(" "));
 };
 
 module.exports.conf = {
