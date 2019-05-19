8503b1e4cf2e74768520cae3b9b9d6fea376a19d
Julien Ternet <snoupy18@gmail.com>
2019-05-19 19:51:57 +0200

    bugs

--- commands/ans.js
+++ commands/ans.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   message.delete();
-  const member = message.guild.members.get("302901933419790347");
-  if (!args) return message.reply("vous devez écrire un message !");
+  const member = message.guild.members.get(args[0]);
+  if (!args[0]) return message.reply("vous devez préciser une ID !");
+  if (isNaN(args[0])) return message.reply("l'ID est invalide. ");
 
   const embed = new Discord.RichEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setTitle(`${message.author.tag} (ID : ${message.author.id})`)
     .setColor("RANDOM")
-    .setDescription(args.join(" "))
+    .setDescription(args.join(" ").slice(19))
     .setFooter(
       "Ce bot a été créé par Spokloo#7791",
       "https://i.imgur.com/C7cjSEe.png"
@@ -23,10 +24,10 @@ module.exports.run = async (bot, message, args) => {
 module.exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["dm", "message", "msg"],
+  aliases: ["answer", "rep"],
   permLevel: 0
 };
 
 module.exports.help = {
-  name: "mp"
+  name: "ans"
 };
--- cookie.js
+++ cookie.js
@@ -19,57 +19,30 @@ bot.on("message", async message => {
     .split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if (command === "game") {
+  if (command === "ans") {
     message.delete();
-    if (message.author.id !== "302901933419790347") {
-      return message.reply(
-        ":x: Seul le créateur du bot peut utiliser cette commande !"
-      );
-    }
+    const member = message.guild.members.get(args[0]);
+    if (!args[0]) return message.reply("vous devez préciser une ID !");
+    if (isNaN(args[0])) return message.reply("l'ID est invalide. ");
 
-    if (!args[0]) {
-      return message.channel.send(
-        "Syntaxe incorrecte !\nUtilisez : <gamestatus [status]"
-      );
-    }
+    const embed = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.displayAvatarURL)
+      .setTitle(`${message.author.tag} (ID : ${message.author.id})`)
+      .setColor("RANDOM")
+      .setDescription(args.join(" ").slice(19))
+      .setFooter(
+        "Ce bot a été créé par Spokloo#7791",
+        "https://i.imgur.com/C7cjSEe.png"
+      )
+      .setTimestamp();
 
-    if (args[0] === "default") {
-      try {
-        await bot.user.setPresence({
-          game: {
-            name: "<help",
-            type: "PLAYING"
-            //url: 'https://www.twitch.tv/spokloo'
-          },
-          status: "online"
-        });
-      } catch (error) {
-        console.error(error);
-      }
-      return message.channel.send(
-        "Le message de jeu a été changé en `default` avec succès !"
+    try {
+      await member.send(embed);
+      message.author.send("Le message a bien été envoyé au correspondant !");
+    } catch (e) {
+      message.author.send(
+        "L'utilisateur a désactivé ses messages privés, le message ne lui a donc pas été envoyé."
       );
-    } else {
-      try {
-        await bot.user.setPresence({
-          game: {
-            name: `${args.join(" ")}`,
-            type: "PLAYING"
-          },
-          status: "online"
-        });
-
-        return message.channel.send(
-          `Le message de jeu a été changé en \`${args.join(
-            " "
-          )}\` avec succès !`
-        );
-      } catch (e) {
-        console.error(e);
-        return message.channel.send(
-          "Une erreur s'est produite, veuillez réessayer !"
-        );
-      }
     }
   }
 });
