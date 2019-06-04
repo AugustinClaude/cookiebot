1f7d2a0cbf9f61a04b0adf2a02691220eea79a29
Julien Ternet <snoupy18@gmail.com>
2019-06-04 23:16:17 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -3,16 +3,15 @@ const arraySort = require("array-sort");
 
 module.exports.run = async (bot, message) => {
   message.delete();
-  bot.guilds.forEach(guild => {
+  const array = bot.guilds.forEach(guild => {
     console.log(guild.memberCount);
   });
-
-  /*const filtered = bot.guilds.array();
-  console.log(filtered + "\n----------------------\n");
-  const sorted = filtered.sort((a, b) => a.points - b.points);
+  //const filtered = bot.guilds.array();
+  console.log(array + "\n----------------------\n");
+  const sorted = array.sort((a, b) => a.points - b.points);
   console.log(sorted + "\n----------------------\n");
   const top10 = sorted.splice(0, 10).reverse();
-  console.log(top10 + "\n----------------------\n");*/
+  console.log(top10 + "\n----------------------\n");
   const embed = new Discord.RichEmbed()
     .setTitle("Leaderboard")
     .setAuthor(message.guild.name, message.guild.iconURL)
@@ -21,12 +20,12 @@ module.exports.run = async (bot, message) => {
     )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
-  /*for (const data of top10) {
+  for (const data of top10) {
     embed.addField(
       bot.guilds.get(data.guild),
       `**${data.channels}** channels ║ **${data.users}** users`
     );
-  }*/
+  }
   return message.channel.send(embed);
 };
 
