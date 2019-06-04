dbfc9199be062dc66b362f52f81b1d53ed994a0f
Julien Ternet <snoupy18@gmail.com>
2019-06-04 17:47:17 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -2,22 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message) => {
   message.delete();
-
   const filtered = bot.guilds.filter(p => p.guild === message.guild.id).array();
-  const sorted = filtered.sort((a, b) => a.guilds - b.guilds);
+  console.log(filtered + "OK DAKO");
+  const sorted = filtered.sort((a, b) => a.points - b.points);
+  console.log(sorted + "OK DAKO");
   const top10 = sorted.splice(0, 10).reverse();
+  console.log(top10 + "OK DAKO");
   const embed = new Discord.RichEmbed()
     .setTitle("Leaderboard")
     .setAuthor(message.guild.name, message.guild.iconURL)
     .setDescription(
-      "Voici le Top 10 des meilleurs serveurs sur lesquels je me trouve !"
+      "Top 10 des serveurs avec le plus d'utilisateurs sur lesquels je me trouve !"
     )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
   for (const data of top10) {
     embed.addField(
       bot.users.get(data.user).tag,
-      `Niveau ${data.level} (${data.guilds} points)`
+      `Niveau ${data.level} (${data.points} points)`
     );
   }
   return message.channel.send(embed);
