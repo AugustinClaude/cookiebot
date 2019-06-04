77322b1065bad4d7b6f69ee3d54a25353e4dc1f7
Julien Ternet <snoupy18@gmail.com>
2019-06-04 17:39:15 +0200

    bugs

---
+++ commands/topguilds.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message) => {
+  message.delete();
+
+  const filtered = bot.guilds.filter(p => p.guild === message.guild.id).array();
+  const sorted = filtered.sort((a, b) => a.points - b.points);
+  const top10 = sorted.splice(0, 10).reverse();
+  const embed = new Discord.RichEmbed()
+    .setTitle("Leaderboard")
+    .setAuthor(message.guild.name, message.guild.iconURL)
+    .setDescription("Voici le Top 10 des joueurs ayant le plus d'xp !")
+    .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
+    .setColor("RANDOM");
+  for (const data of top10) {
+    embed.addField(
+      bot.users.get(data.user).tag,
+      `Niveau ${data.level} (${data.points} points)`
+    );
+  }
+  return message.channel.send(embed);
+};
+
+module.exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["topservers", "top"],
+  permLevel: 0
+};
+
+module.exports.help = {
+  name: "topguilds"
+};
