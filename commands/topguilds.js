5c4250702a66f12fe46734575e85c9d2eb75d457
Julien Ternet <snoupy18@gmail.com>
2019-06-05 22:37:13 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message) => {
   });
 
   const servers = bot.guilds
-    .map(g => "● " + g.name + "**==>**" + g.memberCount + " users")
+    .map(g => "● " + g.name + " ==> " + g.memberCount + " users")
     //.join("\n\n")
     .sort((a, b) => b.array - a.array)
     .splice(0, 10)
