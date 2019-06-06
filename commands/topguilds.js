267e498866e6e59d1b0e7ec645789c2274247406
Julien Ternet <snoupy18@gmail.com>
2019-06-06 18:38:32 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -31,21 +31,23 @@ module.exports.run = async (bot, message) => {
     .setDescription(
       `● **${top[0].name}**\n  => **${top[0].memberCount}** users\n● **${
         top[1].name
-      }**\n  => **${top[1].memberCount}** users\n● **${top[2].name}**\n  => **${
-        top[2].memberCount
-      }** users\n● **${top[3].name}**\n  => **${
-        top[3].memberCount
-      }** users\n● **${top[4].name}**\n  => **${
-        top[4].memberCount
-      }** users\n● **${top[5].name}**\n  => **${
-        top[5].memberCount
-      }** users\n● **${top[6].name}**\n  => **${
-        top[6].memberCount
-      }** users\n● **${top[7].name}**\n  => **${
-        top[7].memberCount
-      }** users\n● **${top[8].name}**\n  => **${
-        top[8].memberCount
-      }** users\n● **${top[9].name}**\n  => **${top[9].memberCount}** users\n`
+      }**\n  => **${top[1].memberCount}** users\n\n● **${
+        top[2].name
+      }**\n  => **${top[2].memberCount}** users\n\n● **${
+        top[3].name
+      }**\n  => **${top[3].memberCount}** users\n\n● **${
+        top[4].name
+      }**\n  => **${top[4].memberCount}** users\n\n● **${
+        top[5].name
+      }**\n  => **${top[5].memberCount}** users\n\n● **${
+        top[6].name
+      }**\n  => **${top[6].memberCount}** users\n\n● **${
+        top[7].name
+      }**\n  => **${top[7].memberCount}** users\n\n● **${
+        top[8].name
+      }**\n  => **${top[8].memberCount}** users\n\n● **${
+        top[9].name
+      }**\n  => **${top[9].memberCount}** users\n`
     )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
