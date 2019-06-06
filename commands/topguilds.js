18b01f3ba71ff1f2290330d934b1a6fb797071a9
Julien Ternet <snoupy18@gmail.com>
2019-06-06 18:36:54 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -28,7 +28,25 @@ module.exports.run = async (bot, message) => {
       "Top 10 des serveurs avec le plus d'utilisateurs sur lesquels je me trouve !"
     )
     .setAuthor(bot.user.username + " ©", bot.user.displayAvatarURL)
-    .setDescription(top[0].name)
+    .setDescription(
+      `● **${top[0].name}**\n  => **${top[0].memberCount}** users\n● **${
+        top[1].name
+      }**\n  => **${top[1].memberCount}** users\n● **${top[2].name}**\n  => **${
+        top[2].memberCount
+      }** users\n● **${top[3].name}**\n  => **${
+        top[3].memberCount
+      }** users\n● **${top[4].name}**\n  => **${
+        top[4].memberCount
+      }** users\n● **${top[5].name}**\n  => **${
+        top[5].memberCount
+      }** users\n● **${top[6].name}**\n  => **${
+        top[6].memberCount
+      }** users\n● **${top[7].name}**\n  => **${
+        top[7].memberCount
+      }** users\n● **${top[8].name}**\n  => **${
+        top[8].memberCount
+      }** users\n● **${top[9].name}**\n  => **${top[9].memberCount}** users\n`
+    )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
   /*for (const data of top10) {
