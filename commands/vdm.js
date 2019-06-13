d2db1302eda7988739723d2612db538366c0474e
Julien Ternet <snoupy18@gmail.com>
2019-06-13 12:51:34 +0200

    bugs

--- commands/vdm.js
+++ commands/vdm.js
@@ -1,4 +1,4 @@
-/*const Discord = require("discord.js");
+const Discord = require("discord.js");
 const request = require("request");
 
 module.exports.run = async (bot, message, args) => {
@@ -28,48 +28,4 @@ module.exports.conf = {
 
 module.exports.help = {
   name: "vdm"
-};*/
-
-const request = require("request");
-const Discord = require("discord.js");
-exports.run = (bot, message, args) => {
-  if (`${args[0]}` == "help") {
-    message.delete(30000);
-    const settingsEmbed = new Discord.RichEmbed()
-      .setDescription(`Cette commande permet de raconte une VDM\n
-    \n-Utilisation : *vdm `);
-    message.channel.send(settingsEmbed).then(msg => {
-      msg.delete(30000);
-    });
-    return;
-  } else {
-    message.delete(300000);
-    // eslint-disable-next-line no-useless-escape
-    const regex = /<p class=\"block hidden-xs\">\n<a href=\".*\">\n(.*) VDM/;
-    request("https://www.viedemerde.fr/aleatoire", (error, response, body) => {
-      if (error) {
-        return console.error(error);
-      }
-      const vdm = regex.exec(body);
-      const inviteEmbed = new Discord.RichEmbed()
-        .setColor("#3ad1b2")
-        .addField("Les vie de merdes ....", vdm[1])
-        .setFooter(message.author.username, bot.user.displayAvatarURL);
-
-      return message.channel.send(inviteEmbed).then(msg => {
-        msg.delete(300000);
-      });
-    });
-  }
-};
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-module.exports.help = {
-  name: "vdm",
-  aliases: []
 };
