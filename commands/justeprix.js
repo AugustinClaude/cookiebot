8e832d0ae5725f9345aa8c8a8df3ac9fa84ad02b
Julien Ternet <snoupy18@gmail.com>
2019-07-05 16:39:14 +0200

    bugs

--- commands/justeprix.js
+++ commands/justeprix.js
@@ -86,10 +86,10 @@ module.exports.run = async (bot, message, args) => {
 module.exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["ggl"],
+  aliases: ["jp"],
   permLevel: 0
 };
 
 module.exports.help = {
-  name: "google"
+  name: "justeprix"
 };
