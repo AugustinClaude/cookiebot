98c647d1db9d4d3a21c2d04ad7aed81fbe742a86
Julien Ternet <snoupy18@gmail.com>
2019-07-09 23:25:36 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -26,6 +26,7 @@ module.exports.run = async (bot, message) => {
     const invite = await channel.createInvite(options);
     invit.push(`https://discord.gg/${invite.code}`);
   });
+  console.log(invit);
 
   const embed = new Discord.RichEmbed()
     .setTitle(
@@ -35,43 +36,43 @@ module.exports.run = async (bot, message) => {
     .setDescription(
       `● **${top[0].name}**\n  => **${
         top[0].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[0])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[0])})\n\n● **${
         top[1].name
       }**\n  => **${
         top[1].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[1])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[1])})\n\n● **${
         top[2].name
       }**\n  => **${
         top[2].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[2])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[2])})\n\n● **${
         top[3].name
       }**\n  => **${
         top[3].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[3])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[3])})\n\n● **${
         top[4].name
       }**\n  => **${
         top[4].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[4])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[4])})\n\n● **${
         top[5].name
       }**\n  => **${
         top[5].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[5])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[5])})\n\n● **${
         top[6].name
       }**\n  => **${
         top[6].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[6])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[6])})\n\n● **${
         top[7].name
       }**\n  => **${
         top[7].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[7])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[7])})\n\n● **${
         top[8].name
       }**\n  => **${
         top[8].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[8])})\n\n● **${
+      }** users \n[[Rejoindre]] (${message.author.send(invit[8])})\n\n● **${
         top[9].name
       }**\n  => **${
         top[9].memberCount
-      }** users \n[[Rejoindre]](${message.author.send(invit[9])})\n`
+      }** users \n[[Rejoindre]] (${message.author.send(invit[9])})\n`
     )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
