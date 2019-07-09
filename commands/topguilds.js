43ef61120c274068aa09b3109a2b1291355afcf4
Julien Ternet <snoupy18@gmail.com>
2019-07-09 20:32:13 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -8,28 +8,23 @@ module.exports.run = async (bot, message) => {
     .array()
     .reverse();
 
+  var options = {
+    maxAge: 3600,
+    maxUses: 1
+  };
   top.forEach(async guild => {
     let channel;
     guild.channels.forEach(chan => {
       channel = chan;
       return;
     });
-    const invite = await channel.createInvite();
-    console.log(invite);
-  });
-
-  /*var options = {
-    maxAge: 3600,
-    maxUses: 1
-  };
-  top.forEach(guild => {
     if (message.author != undefined)
       var invite = guild.defaultChannel
         .createInvite(options)
         .then(function(newInvite) {
           message.author.send("https://discord.gg/" + newInvite.code);
         });
-  });*/
+  });
 
   const embed = new Discord.RichEmbed()
     .setTitle(
