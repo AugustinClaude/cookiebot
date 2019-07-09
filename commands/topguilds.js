7b6bf4d120371ec2ca9448d44626978d44e20684
Julien Ternet <snoupy18@gmail.com>
2019-07-09 23:43:23 +0200

    bugs

--- commands/topguilds.js
+++ commands/topguilds.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message) => {
     maxUses: 1
   };
 
-  const Invit = new Array();
+  const code = [];
   top.forEach(async guild => {
     let channel;
     var index = 0;
@@ -24,9 +24,9 @@ module.exports.run = async (bot, message) => {
       return;
     });
     const invite = await channel.createInvite(options);
-    Invit.push(invite.code);
+    code.push(invite.code);
   });
-  console.log(Invit);
+  console.log(code);
 
   const embed = new Discord.RichEmbed()
     .setTitle(
@@ -36,43 +36,43 @@ module.exports.run = async (bot, message) => {
     .setDescription(
       `● **${top[0].name}**\n  => **${
         top[0].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[0]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[0]})\n\n● **${
         top[1].name
       }**\n  => **${
         top[1].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[1]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[1]})\n\n● **${
         top[2].name
       }**\n  => **${
         top[2].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[2]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[2]})\n\n● **${
         top[3].name
       }**\n  => **${
         top[3].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[3]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[3]})\n\n● **${
         top[4].name
       }**\n  => **${
         top[4].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[4]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[4]})\n\n● **${
         top[5].name
       }**\n  => **${
         top[5].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[5]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[5]})\n\n● **${
         top[6].name
       }**\n  => **${
         top[6].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[6]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[6]})\n\n● **${
         top[7].name
       }**\n  => **${
         top[7].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[7]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[7]})\n\n● **${
         top[8].name
       }**\n  => **${
         top[8].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[8]})\n\n● **${
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[8]})\n\n● **${
         top[9].name
       }**\n  => **${
         top[9].memberCount
-      }** users \n[[Rejoindre]]| (https://discord.gg/${Invit[9]})\n`
+      }** users \n[[Rejoindre]]| (https://discord.gg/${code[9]})\n`
     )
     .setFooter(bot.user.username + " ©", bot.user.displayAvatarURL)
     .setColor("RANDOM");
